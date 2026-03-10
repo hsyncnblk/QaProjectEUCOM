830ba2da4a159adfcd16c3d3c17dcb46a464dded
huseyin cinibulak <huseyincnblk@gmail.com>
2026-03-10 21:52:54 +0300

    git commit -m "build: setup core automation framework with Singleton Driver and ConfigReader"

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,51 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+public class Driver {
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+
+    private Driver() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driverPool.get() == null) {
+            String browser = ConfigReader.getProperty("browser").toLowerCase();
+
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--disable-notifications");
+                    chromeOptions.addArguments("--start-maximized");
+                    driverPool.set(new ChromeDriver(chromeOptions));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    driverPool.set(new FirefoxDriver(firefoxOptions));
+                    break;
+                default:
+                    throw new RuntimeException("Geçersiz tarayıcı tipi: " + browser);
+            }
+
+            int impWait = Integer.parseInt(ConfigReader.getProperty("implicit_wait"));
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(impWait));
+        }
+        return driverPool.get();
+    }
+
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
